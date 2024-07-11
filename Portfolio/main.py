af5c01d021b05be1acb044b64e94435d5dcd24d7
Airel King <airelking@gmail.com>
2024-07-11 06:51:09 -0400

    Added all app descriptions, images and links

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -1,5 +1,4 @@
-import streamlit as st
-
+import pandas
 import streamlit as st
 
 st.set_page_config(layout = "wide")
@@ -22,3 +21,22 @@ Below you can find some of the apps I have built in Python.
 Feel free to contact me!"""
 
 st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("/Users/airelking/Desktop/NewRising/Projects/Portfolio/portfolio_list_data.csv", sep = ";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("py_portfolio_images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("py_portfolio_images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
