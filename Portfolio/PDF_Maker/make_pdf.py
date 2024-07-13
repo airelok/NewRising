4625b8389177e3e6ca8315b9749d4794c3516365
Airel King <airelking@gmail.com>
2024-07-13 14:13:13 -0400

    Updates were made to the header and footer

--- Portfolio/PDF_Maker/make_pdf.py
+++ Portfolio/PDF_Maker/make_pdf.py
@@ -11,20 +11,26 @@ df = pd.read_csv("/Users/airelking/Desktop/NewRising/Projects/Portfolio/PDF_Make
 for index, row in df.iterrows():
     pdf.add_page()
 
+    # set the header
     pdf.set_font(family = "Times", style = "B", size = 24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w = 0, h = 12, txt = row["Topic"], align = "L",
              ln = 1)
-    pdf.line(x1 =10, y1 = 20 , x2 =200, y2 = 20)
+    pdf.line(x1 = 10, y1 = 21 , x2 =200, y2 = 21)
 
-    pdf.ln(250)
-
-    pdf.set_font(family="Times", style="I", size=8)
+    # set the footer
+    pdf.ln(265)
+    pdf.set_font(family = "Times", style = "I", size = 8)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
-             ln=1)
+    pdf.cell(w = 0, h = 10, txt = row["Topic"], align = "R")
 
     for i in range(row["Pages"]-1):
         pdf.add_page()
+
+        # set the footer
+        pdf.ln(277)
+        pdf.set_font(family = "Times", style = "I", size = 8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w = 0, h = 10, txt = row["Topic"], align = "R")
 
 pdf.output("output.pdf")
