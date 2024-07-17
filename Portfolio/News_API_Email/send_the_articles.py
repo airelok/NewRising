cb7f0306f62b6cf569cf2deaf905d83a3af97a2e
Airel King <airelking@gmail.com>
2024-07-17 16:23:18 -0400

    Sent an email with titles and descriptions of articles

--- Portfolio/News_API_Email/send_the_articles.py
+++ Portfolio/News_API_Email/send_the_articles.py
@@ -1,4 +1,5 @@
 import requests
+from news_to_email import send_an_email
 
 api_key = "63b79d35895e465b83739d1dc74dbff7"
 url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=63b79d35895e465b83739d1dc74dbff7"
@@ -10,5 +11,12 @@ request = requests.get(url)
 content = request.json()
 
 #   Access the article titles and descriptions
+body = ""
 for article in content["articles"]:
-    print(article["title"])
+    #   Check for articles that have a None Type for article["title"]
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+    print(body)
+
+body = body.encode("utf-8")
+send_an_email(message = body)
