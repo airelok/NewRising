28ebcb030a8cbda4fb869b10d1eb3e7a44895c5c
Airel King <airelking@gmail.com>
2024-07-17 09:31:55 -0400

    Corrections were made to add the missing table headers to Invoice Statements 10001 & 10002

--- Portfolio/PDF_Maker/Invoices_pdf/generate_pdf.py
+++ Portfolio/PDF_Maker/Invoices_pdf/generate_pdf.py
@@ -5,45 +5,49 @@ from pathlib import Path
 
 #   find the filepath for the excel formatted invoice documents
 filepaths = glob.glob("/Users/airelking/Desktop/NewRising/Projects/Portfolio/PDF_Maker/Invoices_pdf/Invoices/Excels/*.xlsx")
+
+#   Iterate over all excel files in the Invoices (Excel) directory
 for filepath in filepaths:
 
-    #   Extract the file name
+    #   Extract the file name without the path and extension
     filename = Path(filepath).stem.split("/")[-1]
     print(f"\n This is the filepath for {filename}: \n {filepath} \n")
 
     #   Initialize the pdf document (create it)
     pdf = FPDF(orientation = "P", unit = "mm", format="A4")
-    pdf.add_page()
+    pdf.add_page()      #   Add a new page to the pdf
 
-    #   Set the header
+    #   Set the header for the Invoice Statement
+    #   Extract the Invoice number
     invoice_nr = filename.split("-")[0]
     pdf.set_font(family = "Times", size = 24, style = "B")
-    pdf.cell(w = 50,  h = 8, txt = f"Invoice nr. {invoice_nr}", ln = 1)
+    pdf.cell(w = 50,  h = 8, txt = f"Invoice nr. {invoice_nr}", ln = 2)
 
     #   Set the subheader
+    #   Extract the Invoice date
     invoice_date = filename.split("-")[1]
     pdf.set_font(family = "Times", size = 18, style = "B")
     pdf.cell(w = 50, h = 8, txt = f"Invoice Date: {invoice_date}", ln = 2)
 
     #   add the invoice data in a table format
     pdf.set_font(family="Times", size=12, style="B")
-    pdf.cell(w=50, h=8, txt="Invoice Transaction Data", ln=2)
+    pdf.cell(w=0, h=8, txt="Invoice Transaction Data", ln=2)
 
     #   Create a dataframe for each excel file
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     print(f"The data frame ---> \n {df}")
 
-    #   Store the table headers in an iterable
+    #   Extract and format column names as table headers
     columns = list(df.columns)
+    header_cols = [item.replace("_", " ").title() for item in columns]
 
-    #   Reformat header for each colum
-    header_cols = [item.replace("_"," ").title() for item in columns]
+    #   Reformat header for each columns
     pdf.cell(w = 30, h = 8, txt = header_cols[0], border = 1)
-    pdf.cell(w = 70, h = 8, txt = header_cols[1], border = 1)
-    pdf.cell(w = 30, h = 8, txt = header_cols[2], border = 1)
+    pdf.cell(w = 50, h = 8, txt = header_cols[1], border = 1)
+    pdf.cell(w = 40, h = 8, txt = header_cols[2], border = 1)
     pdf.cell(w = 30, h = 8, txt = header_cols[3], border = 1)
     pdf.cell(w = 30, h = 8, txt = header_cols[4], border = 1, ln = 1)
-    print(f"This is the header: \n \n {header_cols} \n")
+    print(f"This is the header: \n {header_cols} \n")
 
     #   Add the rows (values) to the table
     for index, row in df.iterrows():
@@ -52,15 +56,15 @@ for filepath in filepaths:
 
         #   Moving through each colum add the column name & the required format style
         pdf.cell(w = 30, h = 8, txt = str(row["product_id"]), border = 1)
-        pdf.cell(w = 70, h = 8, txt = str(row["product_name"]), border = 1)
-        pdf.cell(w = 30, h = 8, txt = str(row["amount_purchased"]), border = 1)
+        pdf.cell(w = 50, h = 8, txt = str(row["product_name"]), border = 1)
+        pdf.cell(w = 40, h = 8, txt = str(row["amount_purchased"]), border = 1)
         pdf.cell(w = 30, h = 8, txt = str(row["price_per_unit"]), border = 1)
         pdf.cell(w = 30, h = 8, txt = str(row["total_price"]), border = 1)
 
         pdf.ln()
 
-#   generate the pdf file and name it as follows
-pdf_output = pdf.output(f"Invoices/PDFs/{filename}.pdf")
+    #   generate the pdf file and name it as follows
+    pdf_output = pdf.output(f"Invoices/PDFs/{filename}.pdf")
 
 
 
