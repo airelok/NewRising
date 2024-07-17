60760dbee0fe92337a5bebe91700b0864d3ad35e
Airel King <airelking@gmail.com>
2024-07-17 16:53:02 -0400

    Added links, specified language and limited the number of news articles, add subject and control category

--- Portfolio/News_API_Email/send_the_articles.py
+++ Portfolio/News_API_Email/send_the_articles.py
@@ -1,8 +1,17 @@
 import requests
 from news_to_email import send_an_email
 
+
 api_key = "63b79d35895e465b83739d1dc74dbff7"
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=63b79d35895e465b83739d1dc74dbff7"
+
+topic = "startup"
+
+#   Added a parameter to the url
+url = "https://newsapi.org/v2/top-headlines?" \
+      f"q={topic}&" \
+      "sources=techcrunch&" \
+      "apiKey=63b79d35895e465b83739d1dc74dbff7&" \
+      "language=en"
 
 #   Creates a request object type
 request = requests.get(url)
@@ -12,10 +21,13 @@ content = request.json()
 
 #   Access the article titles and descriptions
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:3]:
     #   Check for articles that have a None Type for article["title"]
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = "Subject: Today's News" + "\n" \
+               + body + article["title"] + "\n" \
+               + article["description"] + "\n" \
+               + article["url"] + 2 * "\n"
     print(body)
 
 body = body.encode("utf-8")
